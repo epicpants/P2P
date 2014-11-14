644e784554d3e0bde6a81e866e9225a8b100de59
Chris Rawlings <cervz2@mst.edu>
2014-11-14 00:46:24 -0600

    added CLI parsing and functions called by CLI parser

--- client.cpp
+++ client.cpp
@@ -24,6 +24,12 @@ using namespace std;
 #define MAX_THREAD_COUNT 1000
 #define PIECE_SIZE 1024
 
+bool createTracker(string fileName);
+bool updateTracker(string fileName);
+bool getList();
+bool getTracker(string getTrackerFileName);
+
+
 //create thread for handling simultaneous input & output
 pthread_t gatherThread;
 pthread_mutex_t mutex=PTHREAD_MUTEX_INITIALIZER;
@@ -90,18 +96,106 @@ void getSegment(void* arg)
 	pthread_exit(0);
 }
 
+void *commandLineInput(void *arg)
+{
+	cout<<"P2P Client CLI Started"<<endl;
+	string userCommandInputString;
+	string userCommand;
+
+	string trackerFileName;
+	while(1)
+  	{
+  		getline(cin, userCommandInputString);
+    	stringstream userCommandStringStream(userCommandInputString);
+    	userCommandStringStream >> userCommand;
+    	if(userCommand == "createtracker" || userCommand == "CREATETRACKER")
+    	{
+    		cout<<"createtracker"<<endl;
+    		userCommandStringStream >> trackerFileName;
+			createTracker(trackerFileName);
+    	}
+    	else if (userCommand == "list" || userCommand == "LIST")
+    	{
+      		cout<<"Getting list of trackers from server"<<endl;
+      		getList();
+    	}
+    	else if (userCommand == "get" || userCommand == "GET")
+    	{
+      		cout<<"GET"<<endl;
+      		userCommandStringStream >> trackerFileName;
+      		getTracker(trackerFileName);
+    	}
+    	else if (userCommand == "updatetracker" || userCommand == "UPDATETRACKER")
+    	{
+      		cout<<"updatetracker"<<endl;
+      		userCommandStringStream >> trackerFileName;
+      		updateTracker(trackerFileName);
+    	}
+    	else
+    	{
+      		cout<<"Command not recognized"<<endl;
+    	}
+  	}	
+}
 
 int main(int argc, char* argv[])
 {
-  //checks to see if the correct number of arguments were passed in
-  if(argc != 2)
-  {
-    cout<<"Usage: "<<argv[0]<<" ip address"<<endl;
-    exit(1);
-  }
+ 	//checks to see if the correct number of arguments were passed in
+ 	if(argc != 2)
+	{
+		cout<<"Usage: "<<argv[0]<<" ip address"<<endl;
+		exit(1);
+	}
 
-  while(1)
-  {
-    
-  }
+	while(1)
+		{
+
+		}
+}
+
+bool createTracker(string fileName)
+{
+	string createTrackerServerCommand;
+	createTrackerServerCommand = TrackerFile::createCommand(fileName.c_str(), PEER_PORT, "Boring Description");
+	tf1.create(createTrackerServerCommand.c_str());
+	int useroutputid = pthread_create(&threads[threadCount], NULL, serverinput, &serverSocket);//data from server
+ 	threadCount++;
+  	sendServerCommand((void*)createTrackerServerCommand.c_str());
+  	return true;
+}
+
+bool updateTracker(string fileName)
+{
+	updateTrackerServerCommand;
+	updateTrackerServerCommand = TrackerFile::updateCommand(fileName.c_str(), PEER_PORT);
+  	int useroutputid = pthread_create(&threads[threadCount], NULL, serverinput, NULL);//data from server
+  	threadCount++;
+  	sendServerCommand((void*)updateTrackerServerCommand.c_str());
+  	return true;
+}
+
+bool getList()
+{
+	//open connection to server
+  	//transmit "REQ LIST"
+  	//close connection
+  	int useroutputid = pthread_create(&threads[threadCount], NULL, serverinput, NULL);//data from server
+  	threadCount++;
+  	sendServerCommand((void*)"REQ LIST");
+  	return true;
+}
+
+bool getTracker(string getTrackerFileName)
+{
+	//download tracker from server
+  	//start new thread(getfrompeer) to download file from peers
+  	getFileData getTrackerData;
+  	getTrackerData.getFileDataFileName = getTrackerFileName;
+  	string getTrackerServerCommand = "GET " + getTrackerFileName;
+  	int useroutputid = pthread_create(&threads[threadCount], NULL, serverinput, NULL);//data from server
+  	threadCount++;
+  	//int peerinputid = pthread_create(&threads[threadCount], NULL, peerInput, NULL);//commands & data from peers
+  	//threadCount++;
+  	sendServerCommand((void*)getTrackerServerCommand.c_str());
+  	return true;
 }
