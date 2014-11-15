2d5eb2db1f1a740d9925cea58cf17b14a263f61c
epicpants <tjrg88@mst.edu>
2014-11-14 23:08:59 -0600

    Basic server client communication

--- server.py
+++ server.py
@@ -1,3 +1,23 @@
 import socket
 
+#SERVER_IP = socket.gethostbyname(socket.getfqdn())
+#SERVER_IP = socket.gethostname()
+SERVER_PORT = 7777
+CHUNK_SIZE = 1024
 
+print "Starting server, hostname = {}".format(socket.gethostname())
+#print SERVER_IP
+sd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print "Binding socket to port 7777"
+sd.bind(('', SERVER_PORT))
+print "Listening for clients..."
+sd.listen(1)
+
+(conn, addr) = sd.accept()
+print 'Connection address:', addr
+#while True:
+data = conn.recv(CHUNK_SIZE)
+    #if not data: break
+print "received data:", data
+    #conn.send(data)  # echo
+conn.close()
