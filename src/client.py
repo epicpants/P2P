12c774fbfd9c1c46a51b66747c5d2d4780768198
epicpants <tjrg88@mst.edu>
2014-11-24 17:43:43 -0600

    Updates to update_command, create_command, timer_routine, and get_tracker_file

--- src/client.py
+++ src/client.py
@@ -18,6 +18,7 @@ UPDATE_SLEEP_TIME = 10  # in seconds
 LIST_SLEEP_TIME = 5  # in seconds
 TARGET_FILE = 'picture_wallpaper.jpg'
 CLIENT_IP = socket.gethostname()
+CLIENT_DIR = '.'
 
 
 def command_line_interface():
@@ -33,23 +34,73 @@ def command_line_interface():
 
 
 def req_list():  # return whether list contains TARGET_FILE
+    has_target_file = False
+    num_files = 0
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((server_address, SERVER_PORT))
+    sock.send('REQ LIST')
+    response = sock.recv(CHUNK_SIZE)
+    if not response:
+        return has_target_file
+    match = re.match(r"REP\sLIST\sBEGIN\s(\d+)", response)
+    if match:
+        num_files = match.group(1)
+    count = 1
+    while count <= num_files:
+        response = sock.recv(CHUNK_SIZE)
+        if not response:
+            break
+        if response.find(TARGET_FILE) != -1:
+            has_target_file = True
+        count += 1
+    response = sock.recv(CHUNK_SIZE)
     sock.close()
-    return False
+    return has_target_file
 
 
-def get_tracker_file():  # get command for server
+def get_tracker_file():  # 'get' command for server, return true if got tracker file
+    error = True
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((server_address, SERVER_PORT))
+    sock.send('GET {0}.track'.format(TARGET_FILE))
+    data = sock.recv(CHUNK_SIZE)
+    if not data:
+        return error
+    elif data.find('REP GET BEGIN') == -1:
+        return error
+
+    tracker_filename = '{0}.track'.format(TARGET_FILE)
+    if os.path.isfile(tracker_filename):
+        os.remove(tracker_filename)
+    tracker_file = open(tracker_filename, 'wb')
+    calc_tracker_md5 = hashlib.md5()
+    rec_tracker_md5 = ''
+
+    while True:
+        data = sock.recv(CHUNK_SIZE)
+        if not data:
+            break
+        match = re.match(r"REP\sGET\sEND\s(.+)", data)
+        if match:
+            rec_tracker_md5 = match.group(1)
+            break
+        tracker_file.write(data)
+        calc_tracker_md5.update(data)
+
+    if str(calc_tracker_md5.hexdigest()) == rec_tracker_md5:
+        error = False
     sock.close()
+    tracker_file.close()
+    return error
 
 
-def get_file():  # get command for peers, return true if file download successful
+def get_file():  # 'get' command for peers, return true if file download successful
+    while get_tracker_file():
+        pass
     return True
 
 
-def update_command(start_byte, end_byte):
+def update_command(start_byte, end_byte):  # return whether command was successful
     error = False
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((server_address, SERVER_PORT))
@@ -92,7 +143,7 @@ def listen_for_peers():
         req = connection.recv(CHUNK_SIZE)
         if not req:
             continue
-        match = re.match(r"get\s([^\s]+)\s(.+)", req.lower())
+        match = re.match(r"get\s([^\s]+)\s(\d+)", req.lower())
         if match:
             filename = match.group(1)
             chunk_num = long(match.group(2))
@@ -110,20 +161,24 @@ def listen_for_peers():
         connection.send('REP GET END')
 
 
-def timer_routine():
+def timer_routine(get_update=False):
     while True:
         if client_type == SND:
             # TODO: Make start_byte and end_byte dynamic based on client_num and time
             start_byte = 0
             end_byte = 0
             if not update_command(start_byte, end_byte):  # an error occurred
-                update_command(start_byte, end_byte) # try one more time
+                update_command(start_byte, end_byte)  # try one more time
             time.sleep(UPDATE_SLEEP_TIME)
-        else:
+        elif not get_update:
             has_target_file = req_list()
-            time.sleep(LIST_SLEEP_TIME)
             if has_target_file:
                 break
+            time.sleep(LIST_SLEEP_TIME)
+        else:
+            if not get_tracker_file():  # an error occurred
+                get_tracker_file()  # try one more time
+            time.sleep(UPDATE_SLEEP_TIME)
 
 
 """
@@ -141,10 +196,14 @@ try:
         while create_command():
             pass
         update_thread = Thread(target=timer_routine)  # Thread to update server periodically
+        update_thread.start()
         listen_for_peers()
     else:  # client_type == RCV
         list_thread = Thread(target=timer_routine)  # Thread to request list from server periodically
+        list_thread.start()
         list_thread.join()
+        get_update_thread = Thread(target=timer_routine, args=(True,))
+        get_update_thread.start()
         download_succ = get_file()
         if download_succ:
             print "I am client_{0} and I received the file correctly!".format(client_num)
