d1d01dfe46c72e53b6ac69211527a4ba4669b5d0
epicpants <tjrg88@mst.edu>
2014-11-24 23:31:02 -0600

    updating scripts to adhere to new client argument requirements

--- test_client_rcv.ex
+++ test_client_rcv.ex
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd src/
-python client.py localhost
-python client.py localhost
-python client.py localhost
-python client.py localhost
-python client.py localhost
+python client.py localhost 1 6
+python client.py localhost 1 7
+python client.py localhost 1 8
+python client.py localhost 1 9
+python client.py localhost 1 10
--- test_client_snd.ex
+++ test_client_snd.ex
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd src/
-python client.py localhost
-python client.py localhost
-python client.py localhost
-python client.py localhost
-python client.py localhost
+python client.py localhost 0 1
+python client.py localhost 0 2
+python client.py localhost 0 3
+python client.py localhost 0 4
+python client.py localhost 0 5
