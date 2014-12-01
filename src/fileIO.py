34e111b51a66cd60f45887847445f635c1563bb2
user <lpgy9@mst.edu>
2014-12-01 13:28:17 -0600

    Updated documentation for fileIO.py

--- src/fileIO.py
+++ src/fileIO.py
@@ -1,24 +1,26 @@
-## @package Class implementation for handling config files
+## @package fileIO Class implementation for handling config files
 
-import json, ast, os
+import json
+import ast
+import os
 
 ## Handles basic File I/O for saving/loading configuration dictionaries
 class fileIO:
 	
-	# Default filename
+	## Default filename to use
 	defFile = "./project.conf"
 	
-	# Sample config data
-	config = { "config1" : "A", "config2" : "B", "config3" : "C" }
+	## Sample config data
+	config = {"config1" : "A", "config2" : "B", "config3" : "C"}
 
-	# Unused constructor
-	def __init__( self ): pass
+	## Unused class constructor
+	def __init__(self): pass
 
 	## Saves dictionary to file
 	# param@ self Reference to this class instance
 	# param@ confData Python dictionary containing config data
 	# param@ filename String defining filename and path
-	def saveConfig( self, confData, fileName = defFile ):
+	def saveConfig(self, confData, fileName = defFile):
 		dir = os.path.dirname(fileName)
 		if not os.path.exists(dir):
 			os.makedirs(dir)
@@ -31,7 +33,7 @@ class fileIO:
 	# return@ a dictionary containing configuration pairs
 	# param@ self Reference to this class instance
 	# param@ filename String defining filename and path
-	def loadConfig( self, fileName ):
+	def loadConfig(self, fileName):
 		file = open( fileName, "r" )
 		
 		# contains unwanted 'u' chars
